75fa766d93f448f18890224dadffae1495bece6f
Jim Grabinski <grabinski@users.noreply.github.com>
2019-09-17 07:01:24 -0400

    Update and rename Install-WebApp.ps1 to Update-WebApp.ps1

--- Install-WebApp.ps1
+++
@@ -1,8 +0,0 @@
-# Configure firewall rule
-netsh advfirewall firewall add rule name="http" dir=in action=allow protocol=TCP localport=80
-
-# Install IIS feature
-Install-WindowsFeature Web-Server
-
-# Copy new index.html file
-Set-Content -Path "C:\inetpub\wwwroot\index.html" -Value "Web app instance: $($env:computername)"
